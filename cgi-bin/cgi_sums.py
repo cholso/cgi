55f394b9a0be03f6b5e335caa6d014e33985be04
Chelsea Nayan <chelseanayan@gmail.com>
2020-04-13 19:34:26 -0700

    Submitting cgi activity

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,16 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+operands = form.getlist('operand') # A list of all the operands
+
+# Convert list of strings to integers and then add them together 
+try: 
+    total = sum(map(int, operands))
+    body = "Your total is: {}".format(total)
+except (ValueError, TypeError):
+    body = "Unable to calculate a sum, please provide interger operands."
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
